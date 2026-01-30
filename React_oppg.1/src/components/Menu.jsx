023c41f1eab7ac4de248b43ab6abdcf6159dbd2d
emr102025 <emr102025@gmail.com>
2026-01-30 10:59:56 +0100

    Added sort

--- React_oppg.1/src/components/Menu.jsx
+++ React_oppg.1/src/components/Menu.jsx
@@ -1,11 +1,20 @@
 import Dish from "./Dish";
+
 export default function Menu({ meny }) {
+  const byCategory = ["Forrett", "Hovedrett", "Dessert"];
+  const sortedMenu = [...meny].sort((firstDish, secondDish) => {
+    const dishPosition1 = byCategory.indexOf(firstDish.kategori);
+    const dishPosition2 = byCategory.indexOf(secondDish.kategori);
+
+    return dishPosition1 - dishPosition2;
+  });
+
   return (
     <>
       <section>
         <ul>
-          <h1>Restaurant Menu</h1>
-          {meny.map((item) => (
+          <h1>Menu</h1>
+          {sortedMenu.map((item) => (
             <Dish key={item.id} item={item} />
           ))}
         </ul>
